3f7277e7678460745476e70eb14f6bf9c7128e67
Christopher Hill <cjameshill@gmail.com>
2023-09-12 11:42:37 +0200

    Updated ping back to return body and query

--- src/fasterpay/fasterpay.controller.ts
+++ src/fasterpay/fasterpay.controller.ts
@@ -1,11 +1,11 @@
-import { All, Body, Controller, UseInterceptors } from "@nestjs/common";
+import { All, Body, Controller, Query, UseInterceptors } from "@nestjs/common";
 import { ErrorsInterceptor } from "src/interceptors/exception.interceptor";
 import { LoggingInterceptor } from "src/interceptors/logging.interceptor";
 import { ResponseInterceptor } from "src/interceptors/response.interceptor";
 import { FasterpayService } from "./fasterpay.service";
 
 @UseInterceptors(new LoggingInterceptor())
-@UseInterceptors(new ResponseInterceptor())
+// @UseInterceptors(new ResponseInterceptor())
 @UseInterceptors(new ErrorsInterceptor())
 @Controller("fasterpay")
 export class FasterpayController {
@@ -15,7 +15,9 @@ export class FasterpayController {
         return await this.service.getPaymentForm(body);
     }
     @All("*")
-    async pingback(@Body() body: any) {
-        return body;
+    async pingback(@Body() body: any, @Query() query: any) {
+        console.log("body", JSON.stringify(body));
+        console.log("query", JSON.stringify(query));
+        return { body, query };
     }
 }
